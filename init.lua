d910533ac17c90a1e1c0379dc0820dcde35298e9
guilhermeg2k <guilhermen2k@gmail.com>
2025-04-02 00:16:55 -0300

    Fix formatters

--- init.lua
+++ init.lua
@@ -273,7 +273,7 @@ require('lazy').setup({
   -- Then, because we use the `opts` key (recommended), the configuration runs
   -- after the plugin has been loaded as `require(MODULE).setup(opts)`.
 
-  { -- Useful plugin to show you pending keybinds.
+  {                     -- Useful plugin to show you pending keybinds.
     'folke/which-key.nvim',
     event = 'VeryLazy', -- Sets the loading event to 'VimEnter'
     opts = {
@@ -319,7 +319,7 @@ require('lazy').setup({
 
       -- Document existing key chains
       spec = {
-        { '<leader>c', group = '[C]ode', mode = { 'n', 'x' } },
+        { '<leader>c', group = '[C]ode',     mode = { 'n', 'x' } },
         { '<leader>d', group = '[D]ocument' },
         { '<leader>r', group = '[R]ename' },
         { '<leader>s', group = '[S]earch' },
@@ -359,7 +359,7 @@ require('lazy').setup({
       { 'nvim-telescope/telescope-ui-select.nvim' },
 
       -- Useful for getting pretty icons, but requires a Nerd Font.
-      { 'nvim-tree/nvim-web-devicons', enabled = vim.g.have_nerd_font },
+      { 'nvim-tree/nvim-web-devicons',            enabled = vim.g.have_nerd_font },
     },
     config = function()
       -- Telescope is a fuzzy finder that comes with a lot of different things that
@@ -466,7 +466,7 @@ require('lazy').setup({
       },
     },
   },
-  { 'Bilal2453/luvit-meta', lazy = true },
+  { 'Bilal2453/luvit-meta',   lazy = true },
   {
     -- Main LSP Configuration
     'neovim/nvim-lspconfig',
@@ -478,7 +478,7 @@ require('lazy').setup({
 
       -- Useful status updates for LSP.
       -- NOTE: `opts = {}` is the same as calling `require('fidget').setup({})`
-      { 'j-hui/fidget.nvim', opts = {} },
+      { 'j-hui/fidget.nvim',       opts = {} },
 
       -- Allows extra capabilities provided by nvim-cmp
       'hrsh7th/cmp-nvim-lsp',
@@ -608,10 +608,11 @@ require('lazy').setup({
       -- Show inline diagnostic
       --
       vim.diagnostic.config {
-        virtual_text = true, -- Enable virtual text diagnostics
-        underline = true, -- Underline the text with the diagnostic (optional)
+        virtual_text = true,      -- Enable virtual text diagnostics
+        signs = true,             -- Show diagnostic signs in the sign column (optional, but common)
+        underline = true,         -- Underline the text with the diagnostic (optional)
         update_in_insert = false, -- Update diagnostics only in normal mode (optional, performance)
-        severity_sort = true, -- Sort diagnostics by severity (optional)
+        severity_sort = true,     -- Sort diagnostics by severity (optional)
       }
 
       -- -- Change diagnostic symbols in the sign column (gutter)
@@ -751,12 +752,22 @@ require('lazy').setup({
         }
       end,
       formatters_by_ft = {
-        lua = { 'stylua' },
-        -- Conform can also run multiple formatters sequentially
-        -- python = { "isort", "black" },
-        --
-        -- You can use 'stop_after_first' to run the first available formatter from the list
-        -- javascript = { "prettierd", "prettier", stop_after_first = true },
+        javascript = { 'prettier' },
+        typescript = { 'prettier' },
+        javascriptreact = { 'prettier' },
+        typescriptreact = { 'prettier' },
+        svelte = { 'prettier' },
+        css = { 'prettier' },
+        html = { 'prettier' },
+        json = { 'prettier' },
+        markdown = { 'prettier' },
+      },
+      formatters = {
+        prettier = {
+          command = 'bun',
+          args = { 'prettier', '--stdin-filepath', '$FILENAME' },
+          stdin = true,
+        },
       },
     },
   },
